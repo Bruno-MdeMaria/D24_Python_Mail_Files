d0a824a9754cdeb1c95b98e42084b1d9f1b079f8
Bruno Martins <brunomartinsdemaria@gmail.com>
2022-09-25 18:01:07 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+PLACEHOLDER = "[name]"
+
 with open("./Input/Names/invited_names.txt") as names_file:
     names  = names_file.readlines()  #le o arquivo jogando para dentro da variavel em forma de lista
     print(names)
