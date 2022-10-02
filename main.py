7ecd4d653b4a8dc4b6cf15ef467c1c6208f44943
Bruno Martins <brunomartinsdemaria@gmail.com>
2022-10-02 09:16:59 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,9 @@ PLACEHOLDER = "[name]"
 
 with open("./Input/Names/invited_names.txt") as names_file:
     names  = names_file.readlines()  #le o arquivo jogando para dentro da variavel em forma de lista
-    print(names)
+
+with open("./Input/Letters/starting_letter.txt") as letter_file:
+    letter_contents = letter_file.read() 
+    for name in names: 
+        new_leter = letter_contents.replace(PLACEHOLDER,name) #replace é um método que substitui o que está definito entre (anterior,novo).
+
